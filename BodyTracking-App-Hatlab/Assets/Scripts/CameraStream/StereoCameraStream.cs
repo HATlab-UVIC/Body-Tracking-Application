5eef881f962b6b3be1488e090184037f903f30fd
Brayden Shkwarok <brady82@live.ca>
2023-11-30 16:52:42 -0800

    Modification to how image frames are processed
    Found memory leak in HL2ResearchMode plugin when calling
    GetLRFCameraBuffer(). Modified so it captures front images seperately.
    
    Modified so that images are stored to a queue and sent in order so
    frames are not missed.

--- BodyTracking-App-Hatlab/Assets/Scripts/CameraStream/StereoCameraStream.cs
+++ BodyTracking-App-Hatlab/Assets/Scripts/CameraStream/StereoCameraStream.cs
@@ -22,8 +22,6 @@ public class StereoCameraStream : MonoBehaviour
     TCPClient tcp_client;
     TCPServer tcp_server;
 
-    IntPtr WorldOriginPtr;
-
 #if WINDOWS_UWP && XR_PLUGIN_OPENXR
     SpatialCoordinateSystem _spatialCoordinateSystem;
 #endif
@@ -32,15 +30,15 @@ public class StereoCameraStream : MonoBehaviour
     HL2ResearchMode researchMode;
 #endif
 
-
+    private Queue<byte[]> SpatialImageFrames;
     void Start()
     {
         UnityDebug.Log("StereoCameraStream :: Starting Camera Stream...");
-        UnityDebug.Log("StereoCameraStream :: Initializing HL2ResearchMode Plugin...");
 
         try
         {
 #if ENABLE_WINMD_SUPPORT
+            UnityDebug.Log("StereoCameraStream :: Initializing HL2ResearchMode Plugin...");
             researchMode = new HL2ResearchMode();
 
             researchMode.InitializeSpatialCamerasFront();
@@ -61,50 +59,76 @@ public class StereoCameraStream : MonoBehaviour
 
 #if WINDOWS_UWP && XR_PLUGIN_OPENXR
         _spatialCoordinateSystem = Microsoft.MixedReality.OpenXR.PerceptionInterop.GetSceneCoordinateSystem(UnityEngine.Pose.identity) as SpatialCoordinateSystem;
-
 #endif
+
+        SpatialImageFrames = new Queue<byte[]>();
 
     }
 
 
+    int garbage_count = 1;
+    byte[] sendBytes = null;
+    byte[] LRFImage = null;
+    long ts_unix_left = 0;
+    long ts_unix_right = 0;
     void Update()
     {
-        byte[] spatialImageBytes = null;
-        long ts_left, ts_right;
-#if ENABLE_WINMD_SUPPORT && WINDOWS_UWP
-        SaveSpatialImageEvent(out spatialImageBytes, out ts_left, out ts_right);
-
-        if (TCPClient.tcp_client_connected && spatialImageBytes != null && spatialImageBytes.Length > 0) 
+        if (garbage_count % 60 == 0)
         {
-            SendCameraFrameAsync(spatialImageBytes, ts_left, ts_right);
+            garbage_count++;
+            GC.Collect();
         }
+#if ENABLE_WINMD_SUPPORT && WINDOWS_UWP
+        SaveSpatialImageEvent();
+        if (SpatialImageFrames.Count < 3) SpatialImageFrames.Enqueue(LRFImage);
+
+        if (!TCPClient.tcp_client_connected || TCPClient.client_sending_image_bytes) return;
+        UnityDebug.Log("StereoCameraStream :: Update :: TCP image queue >> " + SpatialImageFrames.Count);
+        if (SpatialImageFrames.Count > 0)
+        {
+            sendBytes = SpatialImageFrames.Dequeue();
+            if (sendBytes.Length > 0) tcp_client.SendSpatialImageAsync(sendBytes, ts_unix_left, ts_unix_right);
+            UnityDebug.Log("StereoCameraStream :: Update :: Image data sent.");
+        }
+
+        LRFImage = null;
+        sendBytes  = null;
+        ts_unix_left = 0;
+        ts_unix_right = 0;
 #endif
     }
 
 
     public void ConnectToRemoteServer()
     {
-        UnityDebug.Log("CameraImageFrameStream :: Starting TCP Client Connection...");
+        UnityDebug.Log("StereoCameraStream :: Starting TCP Client Connection...");
         tcp_client.start_tcp_client_connection();
     }
 
 
-    public void SendCameraFrameAsync(byte[] LRFImage, long ts_unix_left, long ts_unix_right)
+    public void SendCameraFrameAsync(byte[] Image, long ts_left, long ts_right)
     {
-        tcp_client.SendSpatialImageAsync(LRFImage, ts_unix_left, ts_unix_right);
+        tcp_client.SendSpatialImageAsync(Image, ts_left, ts_right);
     }
 
 
 #if ENABLE_WINMD_SUPPORT && WINDOWS_UWP
-    public void SaveSpatialImageEvent(out byte[] LRFImage, out long ts_unix_left, out long ts_unix_right)
+    public void SaveSpatialImageEvent()
     {
-        UnityDebug.Log("CameraImageframeStream :: Front Spatial Cameras :: Saving Left/Right front camera image buffers...");
+        UnityDebug.Log("StereoCameraStream :: Front Spatial Cameras :: Saving Left/Right front camera image buffers...");
 
-        long ts_ft_left, ts_ft_right;
+        long ts_ft_left = 0;
+        long ts_ft_right = 0;
 
         try 
         { 
-            LRFImage = researchMode.GetLRFCameraBuffer(out ts_ft_left, out ts_ft_right);
+            //LRFImage = researchMode.GetLRFCameraBuffer(out ts_ft_left, out ts_ft_right); // MEMORY LEAK ISSUE
+            byte[] leftImage = researchMode.GetLFCameraBuffer(out ts_ft_left);
+            byte[] rightImage = researchMode.GetRFCameraBuffer(out ts_ft_right);
+            LRFImage = new byte[leftImage.Length + rightImage.Length];
+
+            Array.Copy(leftImage, 0, LRFImage, 0, leftImage.Length);
+            Array.Copy(rightImage, 0, LRFImage, leftImage.Length, rightImage.Length);
             
             Windows.Perception.PerceptionTimestamp ts_left = Windows.Perception.PerceptionTimestampHelper.FromHistoricalTargetTime(DateTime.FromFileTime(ts_ft_left));
             Windows.Perception.PerceptionTimestamp ts_right = Windows.Perception.PerceptionTimestampHelper.FromHistoricalTargetTime(DateTime.FromFileTime(ts_ft_right));
@@ -114,14 +138,13 @@ public class StereoCameraStream : MonoBehaviour
         } 
         catch (Exception e) 
         { 
-            UnityDebug.Log("CameraImageFrameStream :: Error :: GetLRFCameraBuffer() issue with method\n" + e); 
+            UnityDebug.Log("StereoCameraStream :: Error :: GetLRFCameraBuffer() issue with method\n" + e); 
             LRFImage = new byte[0];
             ts_unix_left = 0;
             ts_unix_right = 0;
         }
 
-        UnityDebug.Log("CameraImageFrameStream :: Front Spatial Cameras :: Image buffer length\n\nBytes: "+ LRFImage.Length);
-        UnityDebug.Log("CameraImageframeStream :: Front Spatial Cameras :: Left/Right image buffer saved.");
+        UnityDebug.Log("StereoCameraStream :: Front Spatial Cameras :: Left/Right image buffer saved.");
     }
 #endif
 }
